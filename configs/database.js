c0b08cb94ef9044a6743c82f5ffae0444347a514
Muhammad Juan Akbar <mail@mjuan.me>
2017-02-15 23:41:39 +0100

    added first migration, fix cors setup

---
+++ configs/database.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    development: {
+        username: 'root',
+        password: '',
+        database: 'ibus',
+        host: 'localhost',
+        dialect: 'mysql',
+    }
+};
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export default (req, res, next)=>{
+module.exports = (req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Authorization");
