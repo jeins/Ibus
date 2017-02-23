1c3f4b9304a8650723ca3db737a3e2cc9d7ea4ac
Muhammad Juan Akbar <mail@mjuan.me>
2017-02-23 01:12:49 +0100

    added pagination

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,17 +1,37 @@
 'use strict';
 
-const models = require('../models');
+const m = require('../models');
 const express = require('express');
 const uuid = require('uuid/v1');
 const logger = require('../configs/logger');
 const router = express.Router();
 
-router.get('/test', (req, res)=>{
-    res.json({hello: 'world'});
+router.get('/list/:currPage/:limit', (req, res)=>{
+    let attributes = ['id', 'name', 'status', 'sellingPrice'];
+    let currPage = req.params.currPage;
+    let limit = Number(req.params.limit);
+    let offset = Number(currPage * limit);
+
+    m.Products
+        .findAll({
+            attributes: attributes,
+            order: 'createdAt DESC',
+            offset: offset,
+            limit: limit
+        })
+        .then((products)=>{
+            logger.log('info', 'get products | currPage: %s | limit: %s', currPage, limit);
+
+            res.json(products);
+        })
+        .catch((err)=>{
+            logger.log('error', 'error on get products | error: %s | currPage: %s | limit: %s', err.message, currPage, limit);
+            res.status(500).send({error:true, message: err.message});
+        });
 });
 
-router.post('/create', (req, res)=>{
-    models.Products
+router.post('/add', (req, res)=>{
+    m.Products
         .create({
             id: uuid(),
             name: req.body.name,
