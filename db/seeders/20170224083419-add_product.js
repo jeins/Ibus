e9ad89463305fca2a96b0ce26f70b37dc1aa1346
Muhammad Juan Akbar <mail@mjuan.me>
2017-02-24 10:38:09 +0100

    added product seeder

---
+++ db/seeders/20170224083419-add_product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const faker = require('faker');
+const moment = require('moment');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    let arrData = [];
+    let category = ['tas', 'dompet', 'jam'];
+    let status = ['baru', 'bekas'];
+
+    for(let i=0; i<100; i++){
+      arrData.push({
+        name: faker.commerce.productName(),
+        description: faker.company.catchPhrase(),
+        category: category[Math.floor((Math.random() * 3) + 0)],
+        status: status[Math.floor((Math.random() * 2) + 0)],
+        sellingPrice: JSON.stringify({currency: 'rupiah', value: Math.floor((Math.random() * 100000) + 10000000)}),
+        purchasePrice: JSON.stringify({currency: 'euro', value: Math.floor((Math.random() * 20) + 1000)}),
+        image: faker.image.food(),
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt:  moment().format('YYYY-MM-DD HH:mm:ss')
+      });
+    }
+
+      return queryInterface.bulkInsert('Products', arrData, {});
+  }
+  ,
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
